3a6fe81f0fdc105788010dbf818714f710436969
Sergey Vergun <54082863+svergun@users.noreply.github.com>
2025-04-10 00:21:14 +0200

    Fix for Tag Association in vSphere Provider 2.x+

--- main.tf
+++ main.tf
@@ -11,6 +11,13 @@ resource "vsphere_virtual_machine" "vm" {
   num_cpus                   = each.value.vm_num_cpus
   memory                     = each.value.vm_memory
 
+
+  # Apply tags directly to the VM resource
+  tags = [
+    for tag_category, tag_name in lookup(each.value, "vm_tags", {}) : 
+      data.vsphere_tag.tags["${tag_category}-${tag_name}"].id
+  ]
+
   disk {
     label            = each.value.vm_disk_label
     size             = each.value.vm_disk_size
@@ -27,20 +34,20 @@ resource "vsphere_virtual_machine" "vm" {
 
 }
 
-# Create tag associations for each VM and its tags
-resource "vsphere_tag_association" "vm_tags" {
-  for_each = {
-    for item in flatten([
-      for vm_key, vm in var.vms : [
-        for tag_category, tag_name in lookup(vm, "vm_tags", {}) : {
-          id = "${vm_key}-${tag_category}-${tag_name}"
-          vm_id = vsphere_virtual_machine.vm[vm_key].id
-          tag_key = "${tag_category}-${tag_name}"
-        }
-      ]
-    ]) : item.id => item
-  }
+# # Create tag associations for each VM and its tags
+# resource "vsphere_tag_association" "vm_tags" {
+#   for_each = {
+#     for item in flatten([
+#       for vm_key, vm in var.vms : [
+#         for tag_category, tag_name in lookup(vm, "vm_tags", {}) : {
+#           id = "${vm_key}-${tag_category}-${tag_name}"
+#           vm_id = vsphere_virtual_machine.vm[vm_key].id
+#           tag_key = "${tag_category}-${tag_name}"
+#         }
+#       ]
+#     ]) : item.id => item
+#   }
 
-  tag_ids  = [data.vsphere_tag.tags[each.value.tag_key].id]
-  object_id = each.value.vm_id
-}
+#   tag_ids  = [data.vsphere_tag.tags[each.value.tag_key].id]
+#   object_id = each.value.vm_id
+# }
