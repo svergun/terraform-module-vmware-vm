2a8fb1123406511416929ffc9b19d4b80b6bbd35
Sergey Vergun <54082863+svergun@users.noreply.github.com>
2025-04-10 00:05:00 +0200

    rework tags association

--- data.tf
+++ data.tf
@@ -36,3 +36,29 @@ data "vsphere_content_library_item" "items" {
   type       = var.vmware_content_library_item_type
   library_id = data.vsphere_content_library.library.id
 }
+
+# Get tag categories that we need
+data "vsphere_tag_category" "categories" {
+  for_each = toset(distinct(flatten([
+    for vm in var.vms : keys(lookup(vm, "vm_tags", {}))
+  ])))
+  name = each.key
+}
+
+# Get tags that we need to assign
+data "vsphere_tag" "tags" {
+  for_each = {
+    for tag in flatten([
+      for vm_key, vm in var.vms : [
+        for tag_category, tag_name in lookup(vm, "vm_tags", {}) : {
+          key = "${tag_category}-${tag_name}"
+          category_id = data.vsphere_tag_category.categories[tag_category].id
+          name = tag_name
+        }
+      ]
+    ]) : tag.key => tag
+  }
+
+  name = each.value.name
+  category_id = each.value.category_id
+}
--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-data "vsphere_tag" "tags" {
-  for_each = toset(flatten([
-    for vm in var.vms : [
-      for tag_key, tag_value in vm.vm_tags : format("%s_%s", tag_key, tag_value)
-    ]
-  ]))
-  name        = split("_", each.key)[1]
-  category_id = data.vsphere_tag_category.categories[split("_", each.key)[0]].id
-}
-
-data "vsphere_tag_category" "categories" {
-  for_each = toset([
-    for vm in var.vms : keys(vm.vm_tags)
-  ])
-  name = each.key
-}
 
 # Create the virtual machine
 resource "vsphere_virtual_machine" "vm" {
@@ -43,21 +27,20 @@ resource "vsphere_virtual_machine" "vm" {
 
 }
 
-#Apply tags to the VM
+# Create tag associations for each VM and its tags
 resource "vsphere_tag_association" "vm_tags" {
-  for_each = flatten([
-    for vm_key, vm in var.vms : [
-      for tag_key, tag_value in vm.vm_tags : {
-        vm_id     = vsphere_virtual_machine.vm[vm_key].id
-        tag_key   = tag_key
-        tag_value = tag_value
-        vm_key    = vm_key
-      }
-    ]
-  ])
-  # Use the vsphere_tag data source to get the tag ID
-  tag_ids = [
-    data.vsphere_tag.tags[format("%s_%s", each.value.tag_key, each.value.tag_value)].id
-  ]
+  for_each = {
+    for item in flatten([
+      for vm_key, vm in var.vms : [
+        for tag_category, tag_name in lookup(vm, "vm_tags", {}) : {
+          id = "${vm_key}-${tag_category}-${tag_name}"
+          vm_id = vsphere_virtual_machine.vm[vm_key].id
+          tag_key = "${tag_category}-${tag_name}"
+        }
+      ]
+    ]) : item.id => item
+  }
+
+  tag_ids  = [data.vsphere_tag.tags[each.value.tag_key].id]
   object_id = each.value.vm_id
 }
